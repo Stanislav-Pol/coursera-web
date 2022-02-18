2403cce35521d64614e0d3bce8b59d2cdd800118
Staislav Pol <some453@gmail.com>
2022-02-18 21:33:35 +0200

    step2

--- mod5_solution/chc/js/script.js
+++ mod5_solution/chc/js/script.js
@@ -99,7 +99,7 @@ function buildAndShowHomeHTML (categories) {
     function (homeHtml) {
       console.log("Start function buildAndShowHomeHTML")
       console.log(categories);
-      console.log("Start Step 2");
+      console.log("Start  Step 2");
 
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
