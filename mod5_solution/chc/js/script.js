480ebeaf372d3e4425b0a27110a95aa376d4b48e
Staislav Pol <some453@gmail.com>
2022-02-18 23:08:29 +0200

    4

--- mod5_solution/chc/js/script.js
+++ mod5_solution/chc/js/script.js
@@ -123,7 +123,7 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
       // var homeHtmlToInsertIntoMainPage = ....
-      var homeHtmlToInsertIntoMainPage = $dc.loadMenuItems('VG');
+      var homeHtmlToInsertIntoMainPage = $dc.loadMenuItems('L');
       
       console.log("End of Step 3");
       console.log(homeHtmlToInsertIntoMainPage);
