6423582419bc8a8099f5723ceaf2ba0379feebcd
Staislav Pol <some453@gmail.com>
2022-02-18 22:16:49 +0200

    3

--- mod5_solution/chc/js/script.js
+++ mod5_solution/chc/js/script.js
@@ -123,12 +123,14 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
       // var homeHtmlToInsertIntoMainPage = ....
-
+      var homeHtmlToInsertIntoMainPage = $dc.loadMenuItems("'"+chosenCategoryShortName+"'");
+      console.log("End of Step 3");
 
       // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
       // ....
+      insertHtml("#main-content", homeHtmlToInsertIntoMainPage );
 
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
